151b54214702a80c8a0cbd0efeb8f3d38c984676
Iekrwh <67449064+Iekrwh@users.noreply.github.com>
2022-03-18 18:08:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'blog.iekrhaloo.com' > CNAME
+#echo 'blog.iekrhaloo.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
