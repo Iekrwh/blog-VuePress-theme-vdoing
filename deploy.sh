79a4b9ed434473fc5b53564e444780983cbc3a3c
Iekr <Iekrw23@gmail.com>
2022-03-18 17:45:54 +0800

    编辑Actions自动部署文件

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
+echo 'blog.iekrhaloo.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://Iekrwh:${GITHUB_TOKEN}@github.com/Iekrwh/blog-VuePress-theme-vdoing.git
+  git config --global user.name "Iekrwh"
+  git config --global user.email "iekr_wh@qq.com"
 fi
 git init
 git add -A
