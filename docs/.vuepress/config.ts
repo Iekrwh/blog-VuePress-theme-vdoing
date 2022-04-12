3333b35d4ac408c663b84ef1e6647b73f491fc5b
Iekr <Iekrw23@gmail.com>
2022-04-12 22:44:31 +0800

    添加更多中导航站

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -107,6 +107,13 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '归档', link: '/archives/' },
         ],
       },
+      {
+        text: '更多',
+        // link: '/archives/',
+        items: [
+          { text: '导航站', link: '/navigation/' },
+        ],
+      },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
